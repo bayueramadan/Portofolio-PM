92a00c24a4eef94f863925ed387fd88c55657640
bayueramadan <bayueramadan@gmail.com>
2025-08-15 10:43:10 +0700

    WIP: animasi hero pakai SplitText

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 
 import DataImage, { listProyek } from "./data";
 import { listTools } from "./data";
+import SplitText from "./components/animation/SplitText";
+import GradientText from "./components/animation/GradientText";
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       {/* Hero Section */}
       <div className="hero grid md:grid-cols-2 pt-10 items-center xl:gap-0 gap-6 grid-cols-1">
         <div>
-          <div className="flex items-center gap-3 mb-6 bg-zinc-700 w-fit p-4 rounded-2xl">
+          <div className="flex items-center gap-3 mb-6 bg-aksen2 w-fit p-4 rounded-2xl">
             <img
               src={DataImage.HeroImage}
               alt="Hero Image"
@@ -18,26 +20,40 @@ function App() {
             />
             <q>Rajin Pangkal Pandai🔥</q>
           </div>
-          <h1 className="text-5xl/tight font-bold mb-6">
-            Hi, Saya Bayu Erfan Ramadani
+          <h1>
+            <SplitText
+              text="Hai, Saya Bayu Erfan Ramadani"
+              className="text-2xl font-semibold text-center"
+              delay={100}
+              duration={0.5}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40, delay: 2.2 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={() => {}}
+            />
           </h1>
+
           <p className="text-base/loose mb-6 opacity-50">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            voluptate ratione impedit ut quisquam adipisci accusantium
-            cupiditate error quis eius?
+            Learning the ropes of Product Management through mobile and web
+            projects. I thrive on understanding users, aligning teams, and
+            crafting roadmaps for impactful solutions.
           </p>
           <div className="flex items-center md:gap-4 gap-2">
             <a
               href="https://flowcv.com/resume/m7plm8mhowp4"
-              className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600"
+              className="bg-aksen1 p-4 rounded-2xl hover:bg-violet-600"
             >
-              Download Cv <i class="ri-file-download-line"></i>
+              Download Cv <i className="ri-file-download-line"></i>
             </a>
             <a
               href="#"
               className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600"
             >
-              Lihat Project <i class="ri-archive-drawer-line"></i>
+              Lihat Project <i className="ri-archive-drawer-line"></i>
             </a>
           </div>
         </div>
@@ -48,6 +64,10 @@ function App() {
         />
       </div>
       {/* Hero Section */}
+
+      {/* Study Case section */}
+
+      {/* Study Case section */}
 
       {/* Tentang Section */}
       <div className="tentang mt-30 py-10 ">
@@ -86,7 +106,16 @@ function App() {
 
       {/* Skill Section */}
       <div className="Skill mt-32">
-        <h1 className="text-4xl/snug font-bold mb-4">Skill yang dikuasai</h1>
+        <h1>
+          <GradientText
+            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+            animationSpeed={8}
+            showBorder={false}
+            className="custom-class text-4xl"
+          >
+            Skill Yang Dikuasai
+          </GradientText>
+        </h1>
         <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, optio
           alias. Culpa harum nihil quod amet magnam ratione rem quae!
@@ -121,8 +150,13 @@ function App() {
         </p>
         <div className="proyek-box mt-14 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
           {listProyek.map((proyek) => (
-            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
-              <img src={proyek.gambar} alt="Proyek Image" loading="lazy" />
+            <div key={proyek.id} className="p-4 bg-secondary rounded-md">
+              <img
+                src={proyek.gambar}
+                alt="Proyek Image"
+                loading="lazy"
+                className="rounded-md"
+              />
               <div>
                 <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
                 <p className="text-base/loose mb-4">{proyek.desk}</p>
--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const Preloader = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
+    setTimeout(() => setLoading(false), 2000);
   });
 
   return (
