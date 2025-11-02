e61ae4cee13cd2ab8de73e78dbf5cf8bdb801e79
bayueramadan <bayueramadan@gmail.com>
2025-11-02 18:32:25 +0700

    Background Hero selesai, lanjut ke accordion home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import CustomCursor from "../components/CustomCursor";
 import SplitText from "../Animation/Splittext";
 import { Link } from "react-router-dom";
+import ColorBends from "../Animation/ColorBends";
 
 const handleAnimationComplete = () => {
   console.log("All letters have animated!");
@@ -14,10 +15,31 @@ function Home({ startAnimation }) {
       {/* Custom Cursor */}
       <CustomCursor />
       {/* New Hero Section */}
-      <section className="hero-section px-5 relative h-screen w-screen flex items-center justify-center overflow-hidden bg-warna-gelap">
+      <section className="hero-section relative h-screen w-screen flex items-center justify-center overflow-hidden  px-5">
+        {/* Background animasi */}
+        <div className="absolute inset-0 z-0  ">
+          <ColorBends
+            colors={[
+              "#00f0ff", // cyan aurora
+              "#00ffa3", // mint
+              "#0099ff", // deep sky
+              "#ffb347", // soft orange hue
+            ]}
+            rotation={-60}
+            speed={0.3}
+            autoRotate={2}
+            scale={1.3}
+            frequency={0.83}
+            warpStrength={1}
+            mouseInfluence={0}
+            parallax={0.5}
+            noise={0.1}
+            transparent={false}
+          />
+        </div>
         {/* Konten Hero */}
         <div className="relative z-10 text-center flex flex-col items-center">
-          <h1 className="text-4xl md:text-5xl font-bold text-warna-terang pb-4 mt-20 leading-relaxed">
+          <h1 className="text-4xl md:text-5xl font-bold text-white pb-4 mt-20 leading-relaxed">
             <SplitText
               text="Hello, I'm Bayu Erfan 👋"
               delay={100}
@@ -33,8 +55,11 @@ function Home({ startAnimation }) {
             />
           </h1>
           <h2 className="text-base md:text-3xl font-semibold text-warna-terang/80 mb-6">
-            Focused on learning and applying Product Management in real
-            projects.
+            Focused on learning and applying{" "}
+            <span className="text-warna-hijau font-bold stroke-1">
+              Product Management
+            </span>{" "}
+            in real projects.
           </h2>
 
           <div className="gap-5 flex justify-center">
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import { SitemapPlugin } from "vite-plugin-sitemap";
+import Sitemap from "vite-plugin-sitemap"; // ✅ default import, bukan { SitemapPlugin }
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    SitemapPlugin({
+    Sitemap({
       hostname: "https://www.bayuerfan.com",
       routes: ["/", "/project", "/project/bank-jago", "/about", "/contact"],
     }),
